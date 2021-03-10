021313af7d0464134725d87a6eab0003f717a1b2
Morten Laukvik <morten.laukvik@nav.no>
2021-03-10 12:36:01 +0100

    Bruk sentry

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,15 @@ import App from './App';
 import Modal from 'react-modal';
 import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 import { version } from '../package.json';
+import * as Sentry from '@sentry/browser';
 
 // @ts-ignore
 document.querySelector('meta[name=buildNr]').setAttribute('content', version);
+
+Sentry.init({
+  dsn: 'https://fe8f051bd96d419c95b3c1d414874479@sentry.gc.nav.no/73',
+  environment: 'dev'
+});
 
 Modal.setAppElement('#root');
 
