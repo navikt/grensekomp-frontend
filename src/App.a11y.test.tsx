500dc3b0a5ce9ac9891feddffa3dcb46d0c0e540
Morten Laukvik <morten.laukvik@nav.no>
2021-03-10 13:14:35 +0100

    Ally test på App og mockdata

---
+++ src/App.a11y.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { Application } from './App';
+import { Router } from 'react-router-dom';
+import mockHistory from './mockData/mockHistory';
+
+describe('Application.a11y', () => {
+  it('should have no a11y violations', async () => {
+    const { container } = render(
+      <Router history={mockHistory('/grensekomp')}>
+        <Application />
+      </Router>
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+    cleanup();
+  });
+});
