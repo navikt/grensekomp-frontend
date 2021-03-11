95ebe9d6ad62cc985d1c7476318facd2b7641e81
Kent Fjellstad <kent.fjellstad@nav.no>
2021-03-11 09:42:51 +0100

    Filer

---
+++ src/components/enkelt/EnkeltActions.ts
@@ -0,0 +1,26 @@
+import EnkeltPayload from './EnkeltPayload';
+
+export enum Actions {
+  Reset,
+  Fnr,
+  Orgnr,
+  Fra,
+  Til,
+  Dager,
+  Beloep,
+  Dokumentasjon,
+  Bekreft,
+  Validate,
+  Progress,
+  HandleResponse,
+  Kvittering,
+  NotAuthorized,
+  KontrollDager,
+  AddItem,
+  DeleteItem
+}
+
+export interface EnkeltActions {
+  type: Actions;
+  payload?: EnkeltPayload;
+}
