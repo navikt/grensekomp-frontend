066e453467221e608ee0f7856c3c21ae1a6794dd
Morten Laukvik <morten.laukvik@nav.no>
2021-03-11 11:55:10 +0100

    Mapper klar

--- src/api/bulk/mapBulkRequest.ts
+++ src/api/bulk/mapBulkRequest.ts
@@ -1,21 +1,30 @@
 import BulkRequest from './BulkRequest';
 import BulkState from '../../components/bulk/BulkState';
+import BulkItem from '../../components/bulk/BulkItem';
+import { datoToString } from '../../utils/Dato';
+import Ansatt from './Ansatt';
+
+const mapAnsatt = (item: BulkItem, orgnr: string): Ansatt => ({
+  identitetsnummer: item.fnr || '',
+  virksomhetsnummer: orgnr,
+  perioder: [
+    {
+      fom: datoToString(item.fom),
+      tom: datoToString(item.tom),
+      antallDagerMedRefusjon: parseInt(item?.dager || ''),
+      beloep: parseInt(item.beloep || '')
+    }
+  ]
+});
 
 const mapBulkRequest = (state: BulkState): BulkRequest => {
-  return [
-    {
-      identitetsnummer: 'asf',
-      perioder: [
-        {
-          beloep: 2,
-          antallDagerMedRefusjon: 3,
-          fom: '123',
-          tom: '5124'
-        }
-      ],
-      virksomhetsnummer: 'asv'
-    }
-  ];
+  if (!state.items) {
+    throw new Error('Må ha minst en ansatt');
+  }
+  if (state?.orgnr === undefined) {
+    throw new Error('Må ha orgnr');
+  }
+  return state.items?.map((i) => mapAnsatt(i, state?.orgnr || ''));
 };
 
 export default mapBulkRequest;
