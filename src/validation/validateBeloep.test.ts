b2982c63ab0062f3e018625ae9e10df11d55bc47
Morten Laukvik <morten.laukvik@nav.no>
2021-03-16 16:13:13 +0100

    validateBeløp test

--- src/validation/validateBeloep.test.ts
+++ src/validation/validateBeloep.test.ts
@@ -2,18 +2,26 @@ import validateBeloep from './validateBeloep';
 
 describe('validateBeloep', () => {
   it('should verify that ther is a beloep when required', () => {
-    expect(validateBeloep(123, true)).toBeUndefined();
+    expect(validateBeloep(123, 1000, true)).toBeUndefined();
   });
 
   it('should verify that there is a beloep and not required', () => {
-    expect(validateBeloep(123, false)).toBeUndefined();
+    expect(validateBeloep(123, 1000, false)).toBeUndefined();
+  });
+
+  it('should verify that there beløp is valid', () => {
+    expect(validateBeloep(123, 123, true)).toBeUndefined();
+  });
+
+  it('should verify that there beløp is valid', () => {
+    expect(validateBeloep(123, 122, true)).not.toBeUndefined();
   });
 
   it('should verify that there is a beloep missing when required', () => {
-    expect(validateBeloep(undefined, true)).not.toBeUndefined();
+    expect(validateBeloep(undefined, 1000, true)).not.toBeUndefined();
   });
 
   it('should not verify that there is a beloep missing when not required', () => {
-    expect(validateBeloep(undefined, false)).toBeUndefined();
+    expect(validateBeloep(undefined, 1000, false)).toBeUndefined();
   });
 });
