09db586cec44b47c640900c1400f07efcbaa704d
Morten Laukvik <morten.laukvik@nav.no>
2021-03-16 15:42:30 +0100

    Ikke bruk EnkeltInnsending

--- src/ApplicationRoutes.tsx
+++ src/ApplicationRoutes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import lenker from './config/lenker';
-import Enkeltinnsending from './components/enkelt';
 import Kvittering from './components/kvittering';
 import Bulkinnsending from './components/bulk';
 import TokenFornyet from './components/tokenfornyet';
@@ -10,8 +9,6 @@ import Forside from './components/forside';
 export const ApplicationRoutes = () => (
   <div className='application-routes'>
     <Switch>
-      <Route path={lenker.Enkeltinnsendinng} render={() => <Enkeltinnsending />} />
-      <Route path={lenker.EnkeltKvittering} exact={true} render={() => <Kvittering />} />
       <Route path={lenker.Bulkinnsending} exact={true} render={() => <Bulkinnsending />} />
       <Route path={lenker.BulkKvittering} exact={true} render={() => <Kvittering />} />
       <Route path={lenker.TokenFornyet} exact={true} render={() => <TokenFornyet />} />
