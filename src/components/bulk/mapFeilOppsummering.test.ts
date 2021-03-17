cf415e292ecd0677d7c14a2f252e796357cd0911
Morten Laukvik <morten.laukvik@nav.no>
2021-03-17 09:37:00 +0100

    Ny test: mapFeilOppsummering.test.ts

---
+++ src/components/bulk/mapFeilOppsummering.test.ts
@@ -0,0 +1,45 @@
+import BulkState from './BulkState';
+import mapFeilOppsummering from './mapFeilOppsummering';
+
+describe('mapFeilOppsummering', () => {
+  it('should', () => {
+    const state = {
+      orgnrError: 'feil_orgnr',
+      items: [
+        {
+          uniqueKey: 'feil1',
+          fnrError: 'Feil1_fnr',
+          fomError: 'Feil1_fom',
+          tomError: 'Feil1_tom',
+          dagerError: 'Feil1_dager',
+          beloepError: 'Feil1_beloep'
+        },
+        {
+          uniqueKey: 'feil2',
+          fnrError: 'Feil2_fnr',
+          fomError: 'Feil2_fom',
+          tomError: 'Feil2_tom',
+          dagerError: 'Feil2_dager',
+          beloepError: 'Feil2_beloep'
+        }
+      ]
+    } as BulkState;
+    const feilmeldinger = mapFeilOppsummering(state);
+    expect(feilmeldinger.length).toBe(11);
+    expect(feilmeldinger[0].feilmelding).toBe('feil_orgnr');
+    expect(feilmeldinger[1].skjemaelementId).toBe('fnr_feil1');
+    expect(feilmeldinger[1].feilmelding).toBe('Rad 1: Feil1_fnr');
+
+    expect(feilmeldinger[2].skjemaelementId).toBe('fom_feil1');
+    expect(feilmeldinger[2].feilmelding).toBe('Rad 1: Feil1_fom');
+
+    expect(feilmeldinger[3].skjemaelementId).toBe('tom_feil1');
+    expect(feilmeldinger[3].feilmelding).toBe('Rad 1: Feil1_tom');
+
+    expect(feilmeldinger[4].skjemaelementId).toBe('dager_feil1');
+    expect(feilmeldinger[4].feilmelding).toBe('Rad 1: Feil1_dager');
+
+    expect(feilmeldinger[5].skjemaelementId).toBe('beloep_feil1');
+    expect(feilmeldinger[5].feilmelding).toBe('Rad 1: Feil1_beloep');
+  });
+});
