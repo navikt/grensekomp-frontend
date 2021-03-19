1d87e728d335ccc736a9262a8253aaa22736edb0
Morten Laukvik <morten.laukvik@nav.no>
2021-03-19 09:52:14 +0100

    Fiks feil i dato test

--- src/utils/dato/toDate.test.ts
+++ src/utils/dato/toDate.test.ts
@@ -3,6 +3,6 @@ import { toDate } from './toDate';
 
 describe('toDate', () => {
   it('should map toString', () => {
-    expect(toDate(parseDato('05.10.2020'))).toBe('2020-10-05');
+    expect(toDate(parseDato('05.10.2020')).getTime()).toBe(new Date(2020, 9, 5).getTime());
   });
 });
