eea3c26d3dbf1c7ca1ec402c7f5d0d2299cad587
Morten Laukvik <morten.laukvik@nav.no>
2021-04-12 10:41:17 +0200

    Hjelpetekster for fom og tom

---
+++ src/components/bulk/TilHjelpeLabel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HjelpeLabel from '../felles/HjelpeLabel';
+
+const TilHjelpeLabel = () => (
+  <HjelpeLabel label='Til'>
+    <div style={{ padding: '0.5rem', width: '30rem' }}>
+      Siste arbeidsdag du krever refusjon for på grunn av innreiseforbudet.
+    </div>
+  </HjelpeLabel>
+);
+export default TilHjelpeLabel;
