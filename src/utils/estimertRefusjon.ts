57f95cf03ae93f30284dd278597f44f9b28c5068
Jeppe Feld <jeppe.feld@nav.no>
2021-04-14 16:29:12 +0200

    POH-813 avrunding på estimert refusjon

--- src/utils/estimertRefusjon.ts
+++ src/utils/estimertRefusjon.ts
@@ -3,9 +3,11 @@ const gbeløp: number = 101351; // g-beløp per 29. januar 2021
 const estimertRefusjon = (månedsinntekt: number, ukedager: number): number => {
   const årslønn: number = Math.min(månedsinntekt * 12, gbeløp * 6);
 
-  const dagsats: number = årslønn / 260;
+  const dagsats: number = Math.round(årslønn / 260);
 
-  return Math.round(dagsats * ukedager * 0.7);
+  const vektetDagsats: number = Math.round(dagsats * 0.7);
+
+  return vektetDagsats * ukedager;
 };
 
 export default estimertRefusjon;
