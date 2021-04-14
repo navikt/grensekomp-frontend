2f4437290b1c6fd57127f0bf24c52894ae1532f9
Jeppe Feld <jeppe.feld@nav.no>
2021-04-14 10:20:49 +0200

    Typos

--- src/utils/estimertRefusjon.test.ts
+++ src/utils/estimertRefusjon.test.ts
@@ -1,15 +1,15 @@
 import estimertRefusjon from './estimertRefusjon';
 
 describe('estimertRefusjon', () => {
-  it('should return value for estimate refund', () => {
+  it('should return value for estimated refund', () => {
     expect(estimertRefusjon(25000, 10)).toBe(8077);
   });
 
-  it('should return value for estimate refund (below 6G)', () => {
+  it('should return value for estimated refund (below 6G)', () => {
     expect(estimertRefusjon(50000, 10)).toBe(16154);
   });
 
-  it('should return value for estimate refund (above 6G)', () => {
+  it('should return value for estimated refund (above 6G)', () => {
     expect(estimertRefusjon(100000, 10)).toBe(16372);
   });
 });
