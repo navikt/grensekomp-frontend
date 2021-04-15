601b67b02177a03145ff0a9abacef590d24ab495
Morten Laukvik <morten.laukvik@nav.no>
2021-04-15 15:38:08 +0200

    Rett feil håndtering av timeout tidspunkt

--- src/api/loginexpiry/ParseExpiryDate.test.ts
+++ src/api/loginexpiry/ParseExpiryDate.test.ts
@@ -5,8 +5,10 @@ timezone_mock.register('Europe/London');
 
 describe('ParseExpiryDate', () => {
   it('should return av valid date', () => {
-    const Result = ParseExpiryDate('2021-02-16T14:53:46.000+00:00');
+    expect(ParseExpiryDate('2021-02-16T14:53:46.000+00:00')).toEqual(new Date('2021-02-16 14:53:00'));
+  });
 
-    expect(Result).toEqual(new Date('2021-02-16T14:53:46.000+00:00'));
+  it('should return av valid date', () => {
+    expect(ParseExpiryDate('2021-04-15T14:26:35.000+00:00')).toEqual(new Date('2021-04-15 14:26:00'));
   });
 });
