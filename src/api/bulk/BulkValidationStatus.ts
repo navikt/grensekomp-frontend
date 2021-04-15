15b64300ab42dd734fc1401f3136a1aaedca275a
Kent Fjellstad <kent.fjellstad@nav.no>
2021-04-15 13:14:14 +0200

    Validation errors

--- src/api/bulk/BulkValidationStatus.ts
+++ src/api/bulk/BulkValidationStatus.ts
@@ -1,7 +1,8 @@
 const BulkValidationStatus = {
   OK: 'OK',
   Error: 'VALIDATION_ERRORS',
-  Generic: 'GENERIC_ERROR'
+  Generic: 'GENERIC_ERROR',
+  ValidationErrors: 'VALIDATION_ERRORS'
 };
 
 export default BulkValidationStatus;
