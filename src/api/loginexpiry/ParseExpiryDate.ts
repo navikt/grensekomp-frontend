7b3a2422a9f1103c6e28c8bdac1fe6c0fad7b7d6
Morten Laukvik <morten.laukvik@nav.no>
2021-04-15 15:38:08 +0200

    Rett feil håndtering av timeout tidspunkt

--- src/api/loginexpiry/ParseExpiryDate.ts
+++ src/api/loginexpiry/ParseExpiryDate.ts
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs';
 
-export const ParseExpiryDate = (value: string) => dayjs(value).toDate();
+export const ParseExpiryDate = (value: string): Date => {
+  return dayjs(value.substr(0, 17).replace('T', ' ')).toDate();
+};
 
 export default ParseExpiryDate;
