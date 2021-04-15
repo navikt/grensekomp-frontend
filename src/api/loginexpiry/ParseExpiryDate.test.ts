9ed3d95d42dd8578d0552a0d3d4dbc83310e9311
Morten Laukvik <morten.laukvik@nav.no>
2021-04-15 17:02:00 +0200

    Rett feil med tidssone

--- src/api/loginexpiry/ParseExpiryDate.test.ts
+++ src/api/loginexpiry/ParseExpiryDate.test.ts
@@ -4,11 +4,7 @@ import timezone_mock from 'timezone-mock';
 timezone_mock.register('Europe/London');
 
 describe('ParseExpiryDate', () => {
-  it('should return av valid date', () => {
-    expect(ParseExpiryDate('2021-02-16T14:53:46.000+00:00')).toEqual(new Date('2021-02-16 14:53:00'));
-  });
-
-  it('should return av valid date', () => {
-    expect(ParseExpiryDate('2021-04-15T14:26:35.000+00:00')).toEqual(new Date('2021-04-15 14:26:00'));
+  it('should return vintertid', () => {
+    expect(ParseExpiryDate('2021-01-15T14:26:35.000+00:00')).toEqual(new Date('2021-01-15 14:26:35'));
   });
 });
