fcfb9efe142ed64f9bcca814906400e50f95b265
Morten Laukvik <morten.laukvik@nav.no>
2021-04-15 14:20:39 +0200

    Forbedre test for diffDato

--- src/utils/dato/diffDato.test.ts
+++ src/utils/dato/diffDato.test.ts
@@ -4,4 +4,8 @@ describe('diffDato', () => {
   it('should find correct days', () => {
     expect(diffDate(new Date(2021, 3, 1), new Date(2021, 3, 14))).toBe(10);
   });
+
+  it('should find correct days over three months', () => {
+    expect(diffDate(new Date(2021, 2, 29), new Date(2021, 4, 9))).toBe(30);
+  });
 });
