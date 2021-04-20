90159bd7837fb0d8b6d00d34cf8a55cd59067172
Morten Laukvik <morten.laukvik@nav.no>
2021-04-20 12:40:53 +0200

    Ny tekst for Til hjelpetekst

--- src/components/bulk/TilHjelpeLabel.tsx
+++ src/components/bulk/TilHjelpeLabel.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import HjelpeLabel from '../felles/HjelpeLabel/HjelpeLabel';
 
-const TilHjelpeLabel = () => (
-  <HjelpeLabel label='Til'>Siste arbeidsdag du krever refusjon for på grunn av innreiseforbudet.</HjelpeLabel>
-);
+const TilHjelpeLabel = () => <HjelpeLabel label='Til'>Siste dag i den perioden du krever refusjon for.</HjelpeLabel>;
 export default TilHjelpeLabel;
