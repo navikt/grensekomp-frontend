7070adaf5339eba5f0cdd13de184a4f90352b018
Kent Fjellstad <kent.fjellstad@nav.no>
2021-04-20 15:41:38 +0200

    Fjerner duplikatkode

--- src/utils/dato/parseISODato.ts
+++ src/utils/dato/parseISODato.ts
@@ -1,4 +1,5 @@
 import { Dato } from './Dato';
+import parseDatoVerify from './parseDatoVerify';
 
 const ISO_REGEX = new RegExp('^(\\d{1,4})\\-(\\d{1,2})\\-(\\d{2})$');
 
@@ -13,31 +14,6 @@ export const parseISODato = (date: string): Dato => {
   const year = parseInt(arr[0]);
   const month = parseInt(arr[1]);
   const day = parseInt(arr[2]);
-  const v = new Date(year, month - 1, day);
-  if (v.getDate() != day) {
-    return {
-      value: date,
-      error: 'Ugyldig dato'
-    };
-  }
-  if (v.getMonth() != month - 1) {
-    return {
-      value: date,
-      error: 'Ugyldig måned'
-    };
-  }
-  if (v.getFullYear() != year) {
-    return {
-      value: date,
-      error: 'Ugyldig år'
-    };
-  }
-  return {
-    value: date,
-    day,
-    month,
-    year,
-    millis: v.getTime()
-  };
+  return parseDatoVerify(year, month, day, date);
 };
 export default parseISODato;
