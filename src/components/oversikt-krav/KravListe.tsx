f8c1f68951d3c249de0e61c219ac9273573d2c1b
Jeppe Feld <jeppe.feld@nav.no>
2021-04-20 11:49:43 +0200

    Styling mer lik skisse

--- src/components/oversikt-krav/KravListe.tsx
+++ src/components/oversikt-krav/KravListe.tsx
@@ -1,10 +1,12 @@
 import { Column, Row } from 'nav-frontend-grid';
-import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
 import React from 'react';
 
 import 'nav-frontend-tabell-style';
 import Lenke from 'nav-frontend-lenker';
 import formaterIsoTimestampAsNoTime from '../../utils/formatIsoTimestampAsNoTimestamp';
+import InternLenke from '../felles/InternLenke/InternLenke';
+import lenker from '../../config/lenker';
 
 interface KravListeProps {
   innsendinger: string[];
@@ -16,22 +18,25 @@ const KravListe = (props: KravListeProps) => {
     <>
       <Row>
         <Column>
-          <Sidetittel>Oversikt over tidligere innsendte krav</Sidetittel>
+          <Innholdstittel>Oversikt over tidligere innsendte krav</Innholdstittel>
+          <br />
+          <br />
           <Normaltekst>
-            Har du tidligere søkt om refusjon av sykepenger for fravær som skyldes korona eller tapt arbeidsinntekt på
-            grunn av innreiseforbudet, kan du finne tilbake til dem nedenfor.
+            Har du tidligere søkt om refusjon for{' '}
+            <InternLenke to={lenker.Bulkinnsending}>tapt arbeidsinntekt på grunn av innreiseforbudet</InternLenke>, kan
+            du finne tilbake til dem nedenfor.
           </Normaltekst>
+          <br />
           <hr></hr>
-          <ul>
-            {props.innsendinger &&
-              props.innsendinger.map((item) => (
-                <li key={item}>
-                  <Lenke href='#' onClick={() => props.onKravClick(item)}>
-                    {formaterIsoTimestampAsNoTime(item)}
-                  </Lenke>
-                </li>
-              ))}
-          </ul>
+          {props.innsendinger &&
+            props.innsendinger.map((item) => (
+              <div key={item}>
+                <br />
+                <Lenke href='#' onClick={() => props.onKravClick(item)}>
+                  {formaterIsoTimestampAsNoTime(item)} - Refusjonskrav ved innreiseforbud
+                </Lenke>
+              </div>
+            ))}
         </Column>
       </Row>
     </>
