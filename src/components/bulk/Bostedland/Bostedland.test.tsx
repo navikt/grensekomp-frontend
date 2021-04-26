d77be7ecaabe306220010eb65eaf38a3fde0ba1e
Kent Fjellstad <kent.fjellstad@nav.no>
2021-04-26 15:34:19 +0200

    Test av Bostedland

---
+++ src/components/bulk/Bostedland/Bostedland.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Bostedland from './Bostedland';
+
+describe('Bostedland', () => {
+  it('should display a select box', () => {
+    const mockFn = jest.fn();
+    render(<Bostedland label='label' onChange={mockFn} />);
+
+    expect(screen.queryByText('Finland')).toBeInTheDocument();
+    expect(screen.queryByText('Øvrige land')).toBeInTheDocument();
+    expect(screen.queryByText('Island')).toBeInTheDocument();
+
+    fireEvent.change(screen.getByLabelText('label'), { target: { value: 'ISL' } });
+    expect(mockFn).toHaveBeenCalledWith('ISL');
+  });
+});
