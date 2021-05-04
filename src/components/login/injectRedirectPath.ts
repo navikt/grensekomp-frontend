7aa09eed0659443a9497c4366d2126e2accc99b6
Morten Laukvik <morten.laukvik@nav.no>
2021-05-04 14:13:28 +0200

    POH-863

--- src/components/login/injectRedirectPath.ts
+++ src/components/login/injectRedirectPath.ts
@@ -1,8 +1,7 @@
 import env from '../../config/environment';
 
-function injectRedirectPath(injectedPath: string): string {
+function injectRedirectPath(injectedPath: string, lang: string): string {
   const host = window.location.origin;
-
   return env.loginServiceUrl.replace('XXX', host + injectedPath);
 }
 
