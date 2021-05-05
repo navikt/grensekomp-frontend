2dd172aac3a3a875ef55744269f1b34521ab3af7
Morten Laukvik <morten.laukvik@nav.no>
2021-05-05 14:47:44 +0200

    Mer språkfikser

--- src/locale/mapLocales.test.ts
+++ src/locale/mapLocales.test.ts
@@ -5,7 +5,6 @@ describe('mapLocales', () => {
   it('should map all language keys', () => {
     const langs = mapLocales('nb');
     for (const k in LangKey) {
-      console.log(k);
       expect(langs[k]).not.toBeUndefined();
     }
   });
