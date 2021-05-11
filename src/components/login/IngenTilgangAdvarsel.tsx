eb9eb2a8cf92d6e12c2aab8542d12b0ca8c232e8
Kent Fjellstad <kent.fjellstad@nav.no>
2021-05-11 11:18:46 +0200

    Ubrukte importer og definisjoner

--- src/components/login/IngenTilgangAdvarsel.tsx
+++ src/components/login/IngenTilgangAdvarsel.tsx
@@ -1,12 +1,9 @@
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-import { Link } from 'react-router-dom';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import LangKey from '../../locale/LangKey';
 import Oversettelse from '../felles/Oversettelse/Oversettelse';
 
 export const IngenTilgangAdvarsel = () => {
-  const { t } = useTranslation();
   return (
     <AlertStripeAdvarsel>
       <Oversettelse langKey={LangKey.INGENTILGANGADVARSEL} />
